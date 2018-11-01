76b1283d9866b0b2bd76239aeabe37528e7828d0
wshs <wshs>
2018-11-01 18:27:59 +0800

    试卷做题数的对错统计

--- testKafkaSparkStreaming/src/main/scala/UserPaperSummary.java
+++ testKafkaSparkStreaming/src/main/scala/UserPaperSummary.java
@@ -19,7 +19,7 @@ public class UserPaperSummary {
         // 定义kakfa 服务的地址，不需要将所有broker指定上
         props.put("bootstrap.servers", "master01:9092,slave02:9092,slave03:9092");
         // 制定consumer group
-        props.put("group.id", "answerrecord_offset");
+        props.put("group.id", "user_paper_summary");
         // 是否自动确认offset
         props.put("enable.auto.commit", "true");
         // 自动确认offset的时间间隔
@@ -32,29 +32,34 @@ public class UserPaperSummary {
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
 
         // 指定要消费的topic, 可同时处理多个
-        consumer.subscribe(Arrays.asList("tzanswerrecord_offset"));
+        consumer.subscribe(Arrays.asList("tzuser_paper_summary"));
 
         while (true) {
             // 读取数据，读取超时时间为100ms
             ConsumerRecords<String, String> records = consumer.poll(100);
 
             for (ConsumerRecord<String, String> record : records) {
-                String[] array = record.value().toString().split(",");
+                String[] array = record.value().toString().split("\",\"");
 
                 String id = array[0];
-                int userid = 000000;
+                int userid;
                 int isright;
                 int pid_value;
                 int pid_0_value;
                 int qid_value;
                 int qid_0_value;
+                int h_q_r_count_value = 0;
+                int h_p_r_count_value = 0;
 
-                if (array[1] != null) {
+                System.out.println("".isEmpty());
+                if ( array[1].isEmpty()|| "".equals(array[1])) {
+                    userid = 000000;
+                }else {
                     userid = Integer.parseInt(array[1]);
                 }
-                int testpaperid = Integer.parseInt(array[2]);
-                int questionid = Integer.parseInt(array[3]);
-                int pointid = Integer.parseInt(array[4]);
+                int testpaperid = Integer.parseInt(array[2].toString());
+                int questionid = Integer.parseInt(array[3].toString());
+                int pointid = Integer.parseInt(array[4].toString());
                 String qid = questionid + "";
                 String qid_0 = questionid + "";
                 String pid = pointid + "";
@@ -63,28 +68,37 @@ public class UserPaperSummary {
                 String rowkey = userid + "_" + testpaperid;
                 System.out.println(rowkey);
 
-                if (array[5] == null) {
+                if (array[5].isEmpty()||"\"".equals(array[5])) {
                     isright = 0;
                 } else {
-                    isright = Integer.parseInt(array[5]);
+                    isright = Integer.parseInt(array[5].toString());
                 }
                 int h_pid_0_value = Kafka_hbase.QueryByRowkey("user_paper_summary", rowkey, pid_0);
                 int h_qid_0_value = Kafka_hbase.QueryByRowkey("user_paper_summary", rowkey, qid_0);
+                int h_q_count_value = Kafka_hbase.QueryByRowkey("user_paper_summary", rowkey, "q_count");
+                int h_p_count_value = Kafka_hbase.QueryByRowkey("user_paper_summary", rowkey, "p_count");
+
+
                 if (isright == 0) {
                     pid_0_value = h_pid_0_value + 1;
                     qid_0_value = h_qid_0_value + 1;
-                }else {
-                    pid_0_value = h_pid_0_value ;
-                    qid_0_value = h_qid_0_value ;
+                } else {
+                    pid_0_value = h_pid_0_value;
+                    qid_0_value = h_qid_0_value;
+                    h_q_r_count_value = Kafka_hbase.QueryByRowkey("user_paper_summary", rowkey, "q_r_count");
+                    h_p_r_count_value = Kafka_hbase.QueryByRowkey("user_paper_summary", rowkey, "p_r_count");
+                    h_q_r_count_value = h_q_r_count_value + 1;
+                    h_p_r_count_value = h_p_r_count_value + 1;
                 }
                 int h_pid_value = Kafka_hbase.QueryByRowkey("user_paper_summary", rowkey, pid);
                 int h_qid_value = Kafka_hbase.QueryByRowkey("user_paper_summary", rowkey, qid);
                 pid_value = h_pid_value + 1;
                 qid_value = h_qid_value + 1;
-
+                h_q_count_value = h_q_count_value + 1;
+                h_p_count_value = h_p_count_value + 1;
                 Kafka_hbase.insertDataSum("user_paper_summary", rowkey,
-                        "s", "", "", "", "",
-                        "", "", "", "",
+                        "s", "q_count", h_q_count_value + "", "q_r_count", h_q_r_count_value + "",
+                        "p_count", h_p_count_value + "", "p_r_count", h_p_r_count_value + "",
                         "q", qid_0, qid_0_value + "", qid, qid_value + "",
                         "p", pid_0, pid_0_value + "", pid, pid_value + "");
 
