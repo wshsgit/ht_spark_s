eff84f6a5b488599c2b001cf58d1236bb08f6cf1
wshs <wshs>
2018-11-02 18:05:08 +0800

    数据userpapersummary导入hbase

--- testKafkaSparkStreaming/src/main/scala/UserPaperSummary.java
+++ testKafkaSparkStreaming/src/main/scala/UserPaperSummary.java
@@ -5,6 +5,7 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
@@ -12,7 +13,7 @@ import java.util.Properties;
 
 public class UserPaperSummary {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 //        Kafka_hbase.insertData("t_answerrecord");
 //        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         Properties props = new Properties();
@@ -62,49 +63,57 @@ public class UserPaperSummary {
                 }
                 int testpaperid = Integer.parseInt(array[2].replace("\"", "").toString());
                 int questionid = Integer.parseInt(array[3].replace("\"", "").toString());
-                int pointid = Integer.parseInt(array[4].replace("\"", "").toString());
-                String qid = questionid + "";
-                String qid_0 = questionid + "";
-                String pid = pointid + "";
-                String pid_0 = pointid + "_0";
-                // Rowkey: userid + testpaperid ;
-                String rowkey = userid + "_" + testpaperid;
-                System.out.println(rowkey);
+                ResultScanner results = Kafka_hbase.QueryByCondition1("t_question_point", questionid+"_" );
 
-                if ("".equals(array[5].replace("\"", "").toString())) {
-                    isright = 0;
-                } else {
-                    isright = Integer.parseInt(array[5].replace("\"", "").toString());
+//                System.out.println(results);
+                for (Result result :results) {
+                    System.out.println("获得到rowkey:" + new String(result.getRow()));
+                    String question_point_rowkey = new String(result.getRow());
+                    String[] strings = question_point_rowkey.split("_");
+//                    String pointid = strings[1];
+                    int pointid = Integer.parseInt(strings[1].toString());
+                    String qid = questionid + "";
+                    String qid_0 = questionid + "";
+                    String pid = pointid + "";
+                    String pid_0 = pointid + "_0";
+                    // Rowkey: userid + testpaperid ;
+                    String rowkey = userid + "_" + testpaperid;
+                    System.out.println(rowkey);
+
+                    if ("".equals(array[5].replace("\"", "").toString())) {
+                        isright = 0;
+                    } else {
+                        isright = Integer.parseInt(array[5].replace("\"", "").toString());
+                    }
+                    int h_pid_0_value = Kafka_hbase.QueryByRowkey("user_paper_summary", rowkey, pid_0);
+                    int h_qid_0_value = Kafka_hbase.QueryByRowkey("user_paper_summary", rowkey, qid_0);
+                    int h_q_count_value = Kafka_hbase.QueryByRowkey("user_paper_summary", rowkey, "q_count");
+                    int h_p_count_value = Kafka_hbase.QueryByRowkey("user_paper_summary", rowkey, "p_count");
+
+
+                    if (isright == 0) {
+                        pid_0_value = h_pid_0_value + 1;
+                        qid_0_value = h_qid_0_value + 1;
+                    } else {
+                        pid_0_value = h_pid_0_value;
+                        qid_0_value = h_qid_0_value;
+                        h_q_r_count_value = Kafka_hbase.QueryByRowkey("user_paper_summary", rowkey, "q_r_count");
+                        h_p_r_count_value = Kafka_hbase.QueryByRowkey("user_paper_summary", rowkey, "p_r_count");
+                        h_q_r_count_value = h_q_r_count_value + 1;
+                        h_p_r_count_value = h_p_r_count_value + 1;
+                    }
+                    int h_pid_value = Kafka_hbase.QueryByRowkey("user_paper_summary", rowkey, pid);
+                    int h_qid_value = Kafka_hbase.QueryByRowkey("user_paper_summary", rowkey, qid);
+                    pid_value = h_pid_value + 1;
+                    qid_value = h_qid_value + 1;
+                    h_q_count_value = h_q_count_value + 1;
+                    h_p_count_value = h_p_count_value + 1;
+                    Kafka_hbase.insertDataSum("user_paper_summary", rowkey,
+                            "s", "q_count", h_q_count_value + "", "q_r_count", h_q_r_count_value + "",
+                            "p_count", h_p_count_value + "", "p_r_count", h_p_r_count_value + "",
+                            "q", qid_0, qid_0_value + "", qid, qid_value + "",
+                            "p", pid_0, pid_0_value + "", pid, pid_value + "");
                 }
-                int h_pid_0_value = Kafka_hbase.QueryByRowkey("user_paper_summary", rowkey, pid_0);
-                int h_qid_0_value = Kafka_hbase.QueryByRowkey("user_paper_summary", rowkey, qid_0);
-                int h_q_count_value = Kafka_hbase.QueryByRowkey("user_paper_summary", rowkey, "q_count");
-                int h_p_count_value = Kafka_hbase.QueryByRowkey("user_paper_summary", rowkey, "p_count");
-
-
-                if (isright == 0) {
-                    pid_0_value = h_pid_0_value + 1;
-                    qid_0_value = h_qid_0_value + 1;
-                } else {
-                    pid_0_value = h_pid_0_value;
-                    qid_0_value = h_qid_0_value;
-                    h_q_r_count_value = Kafka_hbase.QueryByRowkey("user_paper_summary", rowkey, "q_r_count");
-                    h_p_r_count_value = Kafka_hbase.QueryByRowkey("user_paper_summary", rowkey, "p_r_count");
-                    h_q_r_count_value = h_q_r_count_value + 1;
-                    h_p_r_count_value = h_p_r_count_value + 1;
-                }
-                int h_pid_value = Kafka_hbase.QueryByRowkey("user_paper_summary", rowkey, pid);
-                int h_qid_value = Kafka_hbase.QueryByRowkey("user_paper_summary", rowkey, qid);
-                pid_value = h_pid_value + 1;
-                qid_value = h_qid_value + 1;
-                h_q_count_value = h_q_count_value + 1;
-                h_p_count_value = h_p_count_value + 1;
-                Kafka_hbase.insertDataSum("user_paper_summary", rowkey,
-                        "s", "q_count", h_q_count_value + "", "q_r_count", h_q_r_count_value + "",
-                        "p_count", h_p_count_value + "", "p_r_count", h_p_r_count_value + "",
-                        "q", qid_0, qid_0_value + "", qid, qid_value + "",
-                        "p", pid_0, pid_0_value + "", pid, pid_value + "");
-
 
                 System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
             }
